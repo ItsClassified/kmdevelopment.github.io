3aaa1af322e424dcb56850f3788ef3054046a939
Kars Miesen <=>
2022-05-09 19:53:42 +0200

    Knop weg

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -39,14 +39,7 @@ export default function Greeting() {
               </p>
               <SocialMedia />
               <div className="button-greeting-div">
-                <Button text="Contact me" href="#contact" />
-                {greeting.resumeLink && (
-                  <Button
-                    text="See my resume"
-                    newTab={true}
-                    href={greeting.resumeLink}
-                  />
-                )}
+                <Button text="Stuur mij een berichtje" href="#contact" />
               </div>
             </div>
           </div>
